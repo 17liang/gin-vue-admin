df0226940b04f58606e5e12404542901c5fe2e29
hongliang7 <hongliang7@staff.weibo.com>
2020-10-26 17:43:35 +0800

    init1026

--- web/vue.config.js
+++ web/vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
     lintOnSave: process.env.NODE_ENV === 'development',
     productionSourceMap: false,
     devServer: {
-        port: 8080,
+        port: 8870,
         open: true,
         overlay: {
             warnings: false,
