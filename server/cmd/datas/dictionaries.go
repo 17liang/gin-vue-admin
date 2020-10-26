f279fc1aa7c772363d100135dea48df12cb0e30b
hongliang7 <hongliang7@staff.weibo.com>
2020-10-26 17:52:52 +0800

    init1026

--- server/cmd/datas/dictionaries.go
+++ server/cmd/datas/dictionaries.go
@@ -9,10 +9,10 @@ import (
 
 type SysDictionaryToPostgresql struct {
 	gorm.Model
-	Name                 string                `json:"name" form:"name" gorm:"column:name;comment:字典名（中）"`
-	Type                 string                `json:"type" form:"type" gorm:"column:type;comment:字典名（英）"`
-	Status               *bool                 `json:"status" form:"status" gorm:"column:status;comment:状态"`
-	Description          string                `json:"description" form:"description" gorm:"column:description;comment:'描述'"`
+	Name                 string                      `json:"name" form:"name" gorm:"column:name;comment:字典名（中）"`
+	Type                 string                      `json:"type" form:"type" gorm:"column:type;comment:字典名（英）"`
+	Status               *bool                       `json:"status" form:"status" gorm:"column:status;comment:状态"`
+	Description          string                      `json:"description" form:"description" gorm:"column:description;comment:'描述'"`
 	SysDictionaryDetails []model.SysDictionaryDetail `json:"sysDictionaryDetails" form:"sysDictionaryDetails"`
 }
 
